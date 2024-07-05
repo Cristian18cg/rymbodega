a96800dcf28df639670a1b1b440fe8bb97731055
Cristian18cg <cristiangarciac2018@gmail.com>
2024-07-05 18:29:14 -0500

    formulario usuarios y incio creacion entregadores

--- gestorbodega/users/urls.py
+++ gestorbodega/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView,CustomTokenObtainPairView
+from .views import RegisterView,CustomTokenObtainPairView, LogoutView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
 ]
--- gestorbodega/users/views.py
+++ gestorbodega/users/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
@@ -22,3 +25,15 @@ class RegisterView(generics.CreateAPIView):
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)    
